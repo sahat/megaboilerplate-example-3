6d695dd89c93c6b6b92b21efc4b6eaab2664c551
Sahat Yalkabov <sakhat@gmail.com>
2016-06-07 22:47:50 -0400

    use window.location.origin for twitter callback url

--- app/actions/oauth.js
+++ app/actions/oauth.js
@@ -7,8 +7,8 @@ import { browserHistory } from 'react-router';
 // Sign in with Twitter
 export function twitterLogin() {
   const twitter = {
-    url: 'http://localhost:3000/auth/twitter',
-    redirectUri: 'http://localhost:3000/auth/twitter/callback',
+    url: window.location.origin + '/auth/twitter',
+    redirectUri: window.location.origin + '/auth/twitter/callback',
     authorizationUrl: 'https://api.twitter.com/oauth/authenticate'
   };
 
